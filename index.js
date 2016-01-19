a104fd8180eb9aaba5fb8ff19b645e1706fb31b4
Jesse Lamb <jesse.lamb@builddirect.com>
2016-01-19 12:15:11 -0800

    rename .gitignore, update eslint rules to support react

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 var fs = require('fs');
 var path = require('path');
 
-// copies a file (`source`) two directory's up as `dest` (optionally `dest` can be omitted)
-// two directory's assumes a folder structure of node_modules/dotfiles/ with this
-// being run as part of npm install from the projects root
+// copies a file (`source`) two directory's up as `dest` (`dest` can be omitted)
+// two directory's assumes a folder structure of node_modules/dotfiles/ with 
+// this being run as part of npm install from the projects root
 function copy( source, dest ) {
-	fs.writeFileSync(path.resolve( __dirname, '../..', dest || source), fs.readFileSync( source ));
+	fs.writeFileSync(path.resolve( __dirname, '../..', dest || source), 
+	  fs.readFileSync( source )
+	);
 }
 
 ['.editorconfig', '.eslintrc', 'browserlist'].forEach( function( file) {
 	copy(file);
 });
 
-// npm renames .gitignore to .npmignore so this is a special case
-copy('.gitignore', '.npmignore');
+// So that this repo doesn't ignore files removed the . from .gitignore,
+// when copying it put it back
+copy('.gitignore', '.gitignore');
