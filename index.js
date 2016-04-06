174abdd821ad49985378e812a04c703a869cad4a
Jesse Lamb <jesse.nz@gmail.com>
2016-04-06 16:00:30 -0700

    replaced fs-extra w/ modified version of fs-extra's copySync

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-var fs = require('fs-sync');
+var copy = require('copy-no-overwrite');
 var resolve = require('path').resolve;
 
-// copies values from files array two directory's up, which assumes folder 
-// structure of node_modules/dotfiles/ with this being run as part of npm 
+// copies values from files array two directory's up, which assumes folder
+// structure of node_modules/dotfiles/ with this being run as part of npm
 // install from the projects root. Ideally we would be getting the base path
-// from a process variable or something but haven't figured that out yet 
+// from a process variable or something but haven't figured that out yet
 
 var files = [
 	'.babelrc',
@@ -17,9 +17,9 @@ var files = [
 var basePath = resolve( __dirname, '../..');
 
 files.forEach( function( file ) {
-	fs.copy( file, resolve( basePath, file) );
+	copy( file, resolve( basePath, file) );
 });
 
 // So that this repo doesn't ignore files, removed the . from .gitignore,
 // when copying it put it back
-fs.copy('gitignore', resolve( basePath, '.gitignore' ) );
+copy('gitignore', resolve( basePath, '.gitignore' ) );
